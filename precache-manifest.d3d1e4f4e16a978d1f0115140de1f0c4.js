e67f4a29b5e0fc4500f151baa031524ba4b48934
Sani <aleksandra-gjurova@hotmail.com>
2020-10-31 21:31:42 +0100

    Updates

---
+++ precache-manifest.d3d1e4f4e16a978d1f0115140de1f0c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e985feb4edc227d072347439692b6a",
+    "url": "/cardslist/index.html"
+  },
+  {
+    "revision": "2c9f21eb2afedd6ab603",
+    "url": "/cardslist/static/css/main.4c02dc9b.chunk.css"
+  },
+  {
+    "revision": "cb8df731927b675d2f2d",
+    "url": "/cardslist/static/js/2.f76a1403.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cardslist/static/js/2.f76a1403.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c9f21eb2afedd6ab603",
+    "url": "/cardslist/static/js/main.ba49a6a7.chunk.js"
+  },
+  {
+    "revision": "39ce02aa2c3a565444fa",
+    "url": "/cardslist/static/js/runtime-main.388f9cfd.js"
+  }
+]);
